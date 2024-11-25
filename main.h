32195b881e043d92af081ffa37f465933b4418e0
SpacePistache <lukeholley@hotmail.com>
2024-11-25 13:38:39 +0100

    commit

---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _printf(const char *format, ...);
+
+#endif
+=======
+int _printf(const char *format, ...);
+#endif
+>>>>>>> 716d7335db0750695fbfc17b2cd9af13a26304e9
