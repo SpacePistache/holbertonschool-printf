48bec4712ff05ecaba9915e680c6805ea50b0c90
SpacePistache <lukeholley@hotmail.com>
2024-11-25 14:33:47 +0100

    creation du premier boucle

--- main.h
+++ main.h
@@ -1,11 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-<<<<<<< HEAD
 
 int _printf(const char *format, ...);
 
 #endif
-=======
-int _printf(const char *format, ...);
-#endif
->>>>>>> 716d7335db0750695fbfc17b2cd9af13a26304e9
