a2e8e53cde7507f1e4f4754e4fea373944acc692
SpacePistache <lukeholley@hotmail.com>
2024-11-25 20:59:18 +0100

    iteration, format array

--- printf.c
+++ printf.c
@@ -3,5 +3,19 @@
 
 int _printf(const char *format, ...)
 {
-    printf("%sHello");
+    int i, count = 0;
+
+    va_list args;
+
+    va_start(args, format);
+
+    for (i = 0; format[i] != '\0'; i++)
+    {
+        if (format[i] != %)
+        {
+            _putchar(format[i]);
+        }
+        count += 1; 
+    }
+    return (count);
 }
