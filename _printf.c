545e9f72ed60f95ddfb43a951a371ba92bf0422d
SpacePistache <lukeholley@hotmail.com>
2024-11-26 13:03:33 +0100

    partly functional, char working

--- _printf.c
+++ _printf.c
@@ -1,55 +1,72 @@
-#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
 #include "main.h"
-
+int print_char(va_list args)
+{
+    char c = va_arg(args, int);
+    write(1, &c, 1);
+    return (1);
+}
+int print_string(va_list args)
+{
+    char *str = va_arg(args, char *);
+    int i = 0;
+    if (str == NULL)
+        str = "(null)";
+    while (str[i] != '\0')
+    {
+        write(1, &str[i], 1);
+        i++;
+    }
+    return (i);
+}
+int print_percent(va_list args)
+{
+    (void)args;
+    write(1, "%", 1);
+    return (1);
+}
 int _printf(const char *format, ...)
 {
     va_list args;
-    int count = 0;
-    int i = 0, j;
-    if (!format) 
-        return (-1);
+    int i = 0, count = 0;
+    int j;
+    specifier_t specifiers[] = {
+        {'c', print_char},
+        {'s', print_string},
+        {'%', print_percent},
+        {0, NULL}
+    };
     va_start(args, format);
-    while (format[i] != '\0') {
-        if (format[i] == '%') {
-            i++; 
-            for (j = 0; specifiers[j].format != NULL; j++) {
-                if (*(specifiers[j].format) == format[i]) { 
-                    count += specifiers[j].handler(args);
+    while (format[i] != '\0')
+    {
+        if (format[i] == '%')
+        {
+            i++;
+            j = 0;
+            while (specifiers[j].spec != 0)
+            {
+                if (format[i] == specifiers[j].spec)
+                {
+                    count += specifiers[j].func(args);
                     break;
                 }
+                j++;
             }
-            if (specifiers[j].format == NULL) { 
-                putchar('%');
-                putchar(format[i]);
+            if (specifiers[j].spec == 0)
+            {
+                write(1, "%", 1);
+                write(1, &format[i], 1);
                 count += 2;
             }
-        } else {
-            putchar(format[i]);
+        }
+        else
+        {
+            write(1, &format[i], 1);
             count++;
         }
         i++;
     }
     va_end(args);
-    return count;
-}
-int print_char(va_list args) {
-    char c = va_arg(args, int);
-    putchar(c);
-    return (1);
-}
-int print_string(va_list args) {
-    char *str = va_arg(args, char *);
-    int i = 0;
-    if (!str) 
-        str = "(null)";
-    while (str[i] != '\0') {
-        putchar(str[i]);
-        i++;
-    }
-    return (i);
-}
-int print_percent(va_list args) {
-    (void)args; 
-    putchar('%');
-    return (1);
+    return (count);
 }
