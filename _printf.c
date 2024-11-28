c755742d789f0d404bdbd9814520d70febffca4d
ilmi-p <iveliu96@gmail.com>
2024-11-28 13:33:28 +0000

    c 90 regler

--- _printf.c
+++ _printf.c
@@ -3,18 +3,27 @@
 #include "main.h"
 
 /**
-* _printf - Affiche une chaîne formatée
-* @format: Chaîne de format avec des spécificateurs
-* Return: Nombre de caractères affichés
-*/
+ * _printf - Formats and prints a string
+ * @format: The format string
+ * Return: The number of characters printed
+ */
 int _printf(const char *format, ...)
 {
 va_list args;
 int i = 0, count = 0;
-
 specifier_t specifiers[] = {
-{'c', print_char}, {'s', print_string}, {'%', print_percent},
-{'d', print_int}, {'i', print_int}, {0, NULL} };
+{'c', print_char},
+{'s', print_string},
+{'%', print_percent},
+{'d', print_int},
+{'i', print_int},
+{0, NULL}
+};
+
+if (format == NULL)
+{
+return (-1);  /* Return -1 if format is NULL */
+}
 
 va_start(args, format);
 while (format && format[i] != '\0')
@@ -32,5 +41,6 @@ count++;
 i++;
 }
 va_end(args);
+
 return (count);
 }
