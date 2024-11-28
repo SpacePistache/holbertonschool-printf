d948dccffc734ee3dcf47c65a0a3dab909a9bdb5
SpacePistache <lukeholley@hotmail.com>
2024-11-28 13:38:11 +0100

    Hot fix

--- _printf.c
+++ _printf.c
@@ -103,9 +103,7 @@ int i = 0, count = 0;
 specifier_t specifiers[] = {
 {'c', print_char}, {'s', print_string}, {'%', print_percent},
 {'d', print_int}, {'i', print_int}, {0, NULL} };
-va_start(args, format);
-while (format && format[i] != '\0')
-{
+@@ -105,22 +19,7 @@ while (format && format[i] != '\0')
 if (format[i] == '%')
 {
 i++;
@@ -129,10 +127,7 @@ count += handle_specifier(format[i], args, specifiers);
 }
 else
 {
-write(1, &format[i], 1);
-count++;
-}
-i++;
+@i++;
 }
 va_end(args);
 return (count);
